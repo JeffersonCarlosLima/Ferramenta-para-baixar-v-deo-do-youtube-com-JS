a4e1e1d8548a7d412bef365eb25bf41709e8ef4d
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2024-04-25 16:37:30 -0300

    commit

--- baixar_com_progresso.js
+++ baixar_com_progresso.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 // URL do vídeo do YouTube que você deseja baixar
-const videoURL = 'https://youtu.be/TOUSnA1hZ_Y';
+const videoURL = 'https://youtu.be/_rmTFScrYdQ';
 
 // Diretório onde você deseja salvar o vídeo
-const outputDir = '';
+const outputDir = './download';
 
 // Obtenha informações sobre o vídeo
 ytdl.getInfo(videoURL).then(info => {
@@ -18,27 +18,33 @@ ytdl.getInfo(videoURL).then(info => {
     // Nome do arquivo de saída
     const outputFilename = `${sanitizedTitle}.mp4`;
 
+    // Tamanho total do vídeo em bytes
+    const totalBytes = parseInt(info.formats[0].contentLength);
+
     // Cria uma stream de leitura para o vídeo
-    const videoStream = ytdl(videoURL);
+    const videoStream = ytdl(videoURL, { quality: 'highestvideo' });
 
     // Cria uma stream de escrita para salvar o vídeo
     const outputStream = fs.createWriteStream(`${outputDir}/${outputFilename}`);
 
     // Variáveis para acompanhar o progresso do download
     let downloadedBytes = 0;
-    let totalBytes = parseInt(info.videoDetails.lengthSeconds) * 1000000; // Tamanho do vídeo em bytes
 
     // Evento 'progress' para acompanhar o progresso do download
     videoStream.on('progress', (chunkLength, downloaded, total) => {
         downloadedBytes = downloaded;
-        console.log(`${((downloadedBytes / totalBytes) * 100).toFixed(2)}% baixado`);
+        const percent = Math.min((downloadedBytes / totalBytes) * 100, 100); // Limitando a 100%
+        const progressBarWidth = 50;
+        const progress = Math.round((progressBarWidth * percent) / 100);
+        const emptyProgress = progressBarWidth - progress;
+        const progressBar = '='.repeat(progress) + ' '.repeat(emptyProgress);
+        console.log(`[${progressBar}] ${percent.toFixed(2)}%`);
     });
 
     // Evento 'end' para lidar com o término do download
     videoStream.on('end', () => {
+        console.log('[==================================================] 100.00%');
         console.log('Download concluído!');
-        // Adicione aqui o código para lidar com o que deve acontecer após o término do download
-        // Por exemplo, você pode adicionar código para fechar a stream de escrita ou realizar outras tarefas relacionadas ao download concluído.
     });
 
     // Pipe a stream de leitura para a stream de escrita para iniciar o download

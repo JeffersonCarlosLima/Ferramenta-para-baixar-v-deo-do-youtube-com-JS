7d4c67d44d271754ec7b2bb081985a558fc5bc7e
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2024-04-25 17:25:09 -0300

    commit

--- download_no_navegador.js
+++ download_no_navegador.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
-app.post('/baixar-video', (req, res) => {
-    const linkVideo = req.body.linkVideo;
-    const videoStream = ytdl(linkVideo, { quality: 'highestvideo' });
-    
-    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-    videoStream.pipe(res);
+app.get('/', (req, res) => {
+    res.send('Seu servidor está online.');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000');
+app.post('/baixar-video', async (req, res) => {
+    try {
+        const linkVideo = req.body.linkVideo;
+        const videoInfo = await ytdl.getInfo(linkVideo);
+        const videoStream = ytdl(linkVideo, { quality: 'highestvideo' });
+
+        res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.videoDetails.title}.mp4"`);
+        videoStream.pipe(res);
+    } catch (error) {
+        console.error('Erro ao baixar o vídeo:', error.message);
+        res.status(500).send('Erro ao baixar o vídeo.');
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
 });
